448b004aa15de046508e430f31b3a7ccb0e8354c
Thomas Vachon <vachon@sessionm.com>
2012-07-05 09:54:14 -0400

    Add the ability to pass an array of acceptable query nets

--- manifests/dnscache.pp
+++ manifests/dnscache.pp
@@ -1,18 +1,14 @@
 class djbdns::dnscache (
-$listen_net
+$listen_nets,
 ){
 
   file {'/etc/dnscache/env/IP':
     content => $::ipaddress,
     require => File['iproot'],
   }
-  file {"dnscache-$listen_net":
-    ensure => present,
-    path   => "/etc/dnscache/root/ip/${listen_net}",
-    notify => Service['dnscache'],
-  }
+  djbdns::listen_nets { $listen_nets: }
   file{'iproot':
-    path    => "/etc/dnscache/root/ip/$::ipaddress",
+    path    => "/etc/dnscache/root/ip/${::ipaddress}",
     owner   => root,
     mode    => '0600',
     notify  => Service['dnscache'],
@@ -23,5 +19,12 @@ $listen_net
     provider => daemontools,
     require  => [File['iproot'], Package['daemontools']],
   }
-  
+
+}
+
+define djbdns::listen_nets() {
+  file { "/etc/dnscache/root/ip/${name}":
+    ensure => 'present',
+    notify => Service['dnscache'],
+  }
 }
